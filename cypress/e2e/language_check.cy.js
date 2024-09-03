a70071c40337b3dbbbd980de39f6623e8a2308ad
Yevhen Kislitsyn <yevhen_kislitsyn@epam.com>
2024-09-03 12:56:39 +0300

    Fixed Language test

--- cypress/e2e/language_check.cy.js
+++ cypress/e2e/language_check.cy.js
@@ -1,15 +1,21 @@
 describe('Language Switch to Ukrainian on EPAM', () => {
   it('should navigate to the Ukrainian careers page', () => {
-    cy.visit('/');
-    cy.xpath('//button[@class="location-selector__button"]/div[@class="location-selector__button-arrow-wrapper"]').click();
-    cy.xpath('//ul[@class="location-selector__list"]//a[@lang="uk"]').click();
-    // cy.url().should('include', 'careers.epam.ua');
-  });
+    cy.visit('https://www.epam.com');
 
-  it('should display the "Вакансії" link', () => {
-    cy.visit('https://careers.epam.ua/');
-    cy.xpath('//span[@class="top-navigation__item-text"]/a[contains(text(),"Вакансії")]')
-      .should('be.visible');
-  });
+    cy.get('.location-selector__button .location-selector__button-arrow-wrapper').click();
+    cy.get("a[href='https://careers.epam.ua'][lang='uk']").eq(1).click();
+    cy.on('uncaught:exception', (e) => {
+      if (e.message.includes('Things went bad')) {
+        // We expect this error, ignore it
+        return false;
+      }
+      throw e;
+    });
 
+    cy.origin('https://careers.epam.ua', () => {
+
+      cy.url().should('include', 'careers.epam.ua');
+      cy.get('span a[href="/vacancies"]').should('have.text', 'Вакансії');
+    });
+  });
 });
